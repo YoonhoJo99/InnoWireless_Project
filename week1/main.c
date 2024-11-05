c504849e47d36d4243825c1cd6c47224b691a319
YoonhoJo99 <we424239@gmail.com>
2024-11-06 00:03:39 +0900

    ..

--- week1/main.c
+++ week1/main.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include "STUNExternalIP.h" // STUNExternalIP 헤더 파일 포함
 
 int main() {
-    // 사용할 STUN 서버 설정 (하나의 서버만 사용)
-    struct STUNServer server = {"stun.l.google.com", 19302};
-    
-    // 공인 IP 주소와 포트를 저장할 변수
+    struct STUNServer server = {"stun.l.google.com", 19302}; // 사용할 STUN 서버
     char address[100];
     unsigned short port;
-    
-    // 공인 IP 주소 및 포트 요청
-    int retVal = getPublicIPAddressAndPort(server, address, &port);
-    
-    // 결과 출력
-    if (retVal != 0) {
-        // 요청이 실패한 경우 에러 메시지 출력
-        printf("Failed to retrieve public IP and port from %s. Error: %d\n", server.address, retVal);
-    } else {
-        // 성공적으로 공인 IP와 포트를 가져온 경우 출력
-        printf("Public IP: %s, Public Port: %d\n", address, port);
+
+    // STUN 요청을 30초마다 반복하여 NAT 포트 유지
+    // NAT는 일정 시간 동안 트래픽이 없으면 포트를 해제한다. 너무 길게하면 안 된다.
+    while (1) {
+        int retVal = getPublicIPAddressAndPort(server, address, &port);
+
+        if (retVal == 0) {
+            printf("Public IP: %s, Public Port: %d\n", address, port);
+        } else {
+            printf("Failed to retrieve public IP and port. Error: %d\n", retVal);
+        }
+
+        // 30초 대기 후 STUN 요청 반복
+        sleep(30);
     }
-    
-    return 0; // 프로그램 종료
+
+    return 0;
 }
+
