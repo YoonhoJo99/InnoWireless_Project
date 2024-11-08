d90e8ed36b646d8bd1f0267a0fd3813c72cc2fdc
YoonhoJo99 <we424239@gmail.com>
2024-11-09 05:19:12 +0900

    asd

--- test/tcp_client.c
+++ test/tcp_client.c
@@ -1,3 +1,4 @@
+// tcp_client.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,56 +8,45 @@
 #include <sys/socket.h>
 
 int main(int argc, char *argv[]) {
-    int clientSocket;
-    struct sockaddr_in serverAddr;
-    char buffer[1024];
-    int port;
-
-    // IP와 포트 번호 입력 확인
     if (argc != 3) {
-        printf("Usage: %s <server_ip> <server_port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <server_ip> <port>\n", argv[0]);
         return 1;
     }
-    char *serverIP = argv[1];
-    port = atoi(argv[2]);
+
+    char *server_ip = argv[1];
+    int port = atoi(argv[2]); // 입력받은 포트 번호
+
+    int sock;
+    struct sockaddr_in serverAddr;
 
     // 소켓 생성
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket < 0) {
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
         perror("Socket creation failed");
-        return 1;
+        return -1;
     }
 
     // 서버 주소 설정
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(port);
-    if (inet_pton(AF_INET, serverIP, &serverAddr.sin_addr) <= 0) {
-        perror("Invalid server IP address");
-        close(clientSocket);
-        return 1;
+    if (inet_pton(AF_INET, server_ip, &serverAddr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        return -1;
     }
 
-    // 서버에 연결 시도
-    if (connect(clientSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("Connection failed");
-        close(clientSocket);
-        return 1;
+    // 서버로 연결 시도
+    if (connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        perror("Connection Failed");
+        return -1;
     }
 
-    printf("Connected to the server: %s:%d\n", serverIP, port);
+    printf("Connected to the server at %s:%d\n", server_ip, port);
 
-    // 서버로부터 데이터 수신 (테스트용)
-    int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-    if (bytesReceived > 0) {
-        buffer[bytesReceived] = '\0';  // 문자열 끝에 NULL 추가
-        printf("Received from server: %s\n", buffer);
-    } else {
-        printf("No message received from the server.\n");
-    }
+    // 연결 후 서버와의 데이터 송수신 코드
 
-    // 소켓 종료
-    close(clientSocket);
+    // 소켓 닫기
+    close(sock);
     return 0;
 }
 
--- test/tcp_server.c
+++ test/tcp_server.c
@@ -1,3 +1,4 @@
+// tcp_server.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,27 +7,22 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-#define BUFFER_SIZE 1024
-
 int main(int argc, char *argv[]) {
-    int serverSocket, clientSocket;
-    struct sockaddr_in serverAddr, clientAddr;
-    char buffer[BUFFER_SIZE];
-    socklen_t addr_size;
-    int port;
-
-    // 포트 번호 입력 확인
     if (argc != 2) {
-        printf("Usage: %s <port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
         return 1;
     }
-    port = atoi(argv[1]);
 
-    // 서버 소켓 생성
+    int port = atoi(argv[1]); // 입력받은 포트 번호
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t clientAddrSize = sizeof(clientAddr);
+
+    // 소켓 생성
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     if (serverSocket < 0) {
         perror("Socket creation failed");
-        return 1;
+        return -1;
     }
 
     // 서버 주소 설정
@@ -39,42 +35,30 @@ int main(int argc, char *argv[]) {
     if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
         perror("Bind failed");
         close(serverSocket);
-        return 1;
+        return -1;
     }
 
-    // 연결 대기
-    if (listen(serverSocket, 5) < 0) {
+    // 리슨 시작
+    if (listen(serverSocket, 1) < 0) {
         perror("Listen failed");
         close(serverSocket);
-        return 1;
+        return -1;
     }
     printf("Listening on port %d...\n", port);
 
     // 클라이언트 연결 수락
-    addr_size = sizeof(clientAddr);
-    clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &addr_size);
+    clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
     if (clientSocket < 0) {
         perror("Accept failed");
         close(serverSocket);
-        return 1;
-    }
-    printf("Client connected.\n");
-
-    // 데이터 송수신 루프
-    while (1) {
-        int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-        if (bytesReceived <= 0) {
-            printf("Client disconnected or error occurred.\n");
-            break;
-        }
-        buffer[bytesReceived] = '\0';
-        printf("Received from client: %s\n", buffer);
-
-        // 클라이언트에게 응답
-        send(clientSocket, buffer, bytesReceived, 0);
+        return -1;
     }
 
-    // 소켓 종료
+    printf("Client connected\n");
+
+    // 연결 후 클라이언트와의 데이터 송수신 코드
+
+    // 소켓 닫기
     close(clientSocket);
     close(serverSocket);
     return 0;
