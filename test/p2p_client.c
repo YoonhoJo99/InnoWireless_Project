a9e2da3739bf9d9d0efb4721ac18938e145e4846
YoonhoJo99 <we424239@gmail.com>
2024-11-11 04:33:15 +0900

    1234

--- test/p2p_client.c
+++ test/p2p_client.c
@@ -16,8 +16,9 @@
 #define GOOGLE_STUN_PORT 19302
 #define BUFFER_SIZE 1024
 #define PUNCH_INTERVAL_MS 500
-#define PUNCH_TIMEOUT_SEC 30
+#define PUNCH_TIMEOUT_SEC 60  // 타임아웃 시간 증가
 #define KEEP_ALIVE_INTERVAL 15
+#define PORT_RANGE 1000      // 포트 범위 크게 증가
 
 // STUN Message Types
 #define STUN_BINDING_REQUEST 0x0001
@@ -59,6 +60,7 @@ typedef struct {
     struct sockaddr_in peer_addr;
     int is_connected;
     pthread_mutex_t mutex;
+    uint16_t last_working_port;  // 마지막으로 응답을 받은 포트 저장
 } PunchingContext;
 
 // 랜덤 트랜잭션 ID 생성
@@ -180,12 +182,19 @@ void* connection_maintenance_thread(void *arg) {
     return NULL;
 }
 
-// NAT 홀펀칭 수행
+// NAT 홀펀칭 시도를 위한 단일 메시지 전송
+void send_punch_attempt(int sock, const char* message, struct sockaddr_in *addr) {
+    sendto(sock, message, strlen(message), 0,
+           (struct sockaddr*)addr, sizeof(*addr));
+}
+
+// NAT 홀펀칭 수행 (개선된 버전)
 int perform_nat_punching(int sock, struct sockaddr_in *peer_addr) {
     PunchingContext ctx = {
         .sock = sock,
         .peer_addr = *peer_addr,
-        .is_connected = 0
+        .is_connected = 0,
+        .last_working_port = 0
     };
     pthread_mutex_init(&ctx.mutex, NULL);
 
@@ -194,44 +203,90 @@ int perform_nat_punching(int sock, struct sockaddr_in *peer_addr) {
 
     const char *punch_messages[] = {
         "PUNCH_REQ",
-        "PUNCH_ACK",
-        "PUNCH_SYN",
-        "PUNCH_SYNACK"
+        "PUNCH_ACK"
     };
 
     time_t start_time = time(NULL);
-    int punching_phase = 0;
     struct sockaddr_in recv_addr;
     socklen_t addr_len = sizeof(recv_addr);
     char buffer[BUFFER_SIZE];
     
+    // 논블로킹 모드 설정
     int flags = fcntl(sock, F_GETFL, 0);
     fcntl(sock, F_SETFL, flags | O_NONBLOCK);
 
+    // 초기 포트 범위 설정
+    uint16_t base_port = ntohs(peer_addr->sin_port);
+    uint16_t port_min = (base_port > PORT_RANGE) ? (base_port - PORT_RANGE) : 1024;
+    uint16_t port_max = base_port + PORT_RANGE;
+
+    printf("Starting enhanced NAT hole punching...\n");
+    printf("Target IP: %s\n", inet_ntoa(peer_addr->sin_addr));
+    printf("Initial port range: %d - %d\n", port_min, port_max);
+
+    int received_response = 0;
+    int punch_count = 0;
+    struct sockaddr_in working_addr = *peer_addr;
+
     while (time(NULL) - start_time < PUNCH_TIMEOUT_SEC) {
-        sendto(sock, punch_messages[punching_phase], 
-               strlen(punch_messages[punching_phase]), 0,
-               (struct sockaddr*)peer_addr, sizeof(*peer_addr));
+        // 기본 포트로 시도
+        send_punch_attempt(sock, punch_messages[0], peer_addr);
+        printf("Attempt %d: Trying base port %d\n", ++punch_count, ntohs(peer_addr->sin_port));
 
-        memset(buffer, 0, BUFFER_SIZE);
-        int recv_len = recvfrom(sock, buffer, BUFFER_SIZE, 0,
-                               (struct sockaddr*)&recv_addr, &addr_len);
+        // 마지막으로 작동한 포트로 시도
+        if (ctx.last_working_port != 0) {
+            working_addr.sin_port = htons(ctx.last_working_port);
+            send_punch_attempt(sock, punch_messages[0], &working_addr);
+            printf("Trying last working port: %d\n", ctx.last_working_port);
+        }
 
-        if (recv_len > 0) {
-            if (strcmp(buffer, punch_messages[punching_phase]) == 0) {
-                punching_phase++;
-                if (punching_phase >= sizeof(punch_messages)/sizeof(punch_messages[0])) {
-                    ctx.is_connected = 1;
-                    printf("NAT hole punching successful!\n");
-                    return 0;
+        // 포트 범위 순회
+        for (uint16_t port = port_min; port <= port_max; port += 10) {  // 10포트 간격으로 시도
+            struct sockaddr_in alt_addr = *peer_addr;
+            alt_addr.sin_port = htons(port);
+            
+            send_punch_attempt(sock, punch_messages[0], &alt_addr);
+            printf("Trying port: %d\n", port);
+
+            // 응답 확인 (각 전송 후 짧은 대기)
+            for (int i = 0; i < 5; i++) {
+                memset(buffer, 0, BUFFER_SIZE);
+                int recv_len = recvfrom(sock, buffer, BUFFER_SIZE, 0,
+                                      (struct sockaddr*)&recv_addr, &addr_len);
+
+                if (recv_len > 0) {
+                    ctx.last_working_port = ntohs(recv_addr.sin_port);
+                    printf("Received from %s:%d - %s\n", 
+                           inet_ntoa(recv_addr.sin_addr),
+                           ctx.last_working_port,
+                           buffer);
+
+                    if (strcmp(buffer, punch_messages[0]) == 0) {
+                        // 응답 전송
+                        send_punch_attempt(sock, punch_messages[1], &recv_addr);
+                        received_response = 1;
+                        peer_addr->sin_port = recv_addr.sin_port;  // 작동하는 포트로 업데이트
+                    }
+                    else if (strcmp(buffer, punch_messages[1]) == 0) {
+                        printf("NAT hole punching successful!\n");
+                        printf("Final working port: %d\n", ctx.last_working_port);
+                        ctx.is_connected = 1;
+                        return 0;
+                    }
                 }
+                usleep(10000);  // 10ms 대기
             }
+        }
+
+        if (received_response) {
+            printf("Response received, focusing on port %d\n", ctx.last_working_port);
         }
 
         usleep(PUNCH_INTERVAL_MS * 1000);
     }
 
-    printf("NAT hole punching failed after timeout\n");
+    printf("NAT hole punching failed after %d attempts\n", punch_count);
+    printf("Total time elapsed: %ld seconds\n", time(NULL) - start_time);
     return -1;
 }
 
@@ -302,12 +357,12 @@ int main(int argc, char *argv[]) {
         FD_ZERO(&readfds);
         FD_SET(STDIN_FILENO, &readfds);
         FD_SET(sock, &readfds);
-        
+
         tv.tv_sec = 1;
         tv.tv_usec = 0;
 
         int activity = select(sock + 1, &readfds, NULL, NULL, &tv);
-        
+
         if (activity < 0) {
             perror("Select error");
             break;
@@ -316,7 +371,7 @@ int main(int argc, char *argv[]) {
         if (FD_ISSET(STDIN_FILENO, &readfds)) {
             memset(buffer, 0, BUFFER_SIZE);
             fgets(buffer, BUFFER_SIZE, stdin);
-            
+
             if (sendto(sock, buffer, strlen(buffer), 0,
                       (struct sockaddr*)&peer_addr, sizeof(peer_addr)) < 0) {
                 perror("Send failed");
@@ -327,7 +382,7 @@ int main(int argc, char *argv[]) {
             memset(buffer, 0, BUFFER_SIZE);
             int recv_len = recvfrom(sock, buffer, BUFFER_SIZE, 0,
                                   (struct sockaddr*)&peer_addr, &peer_addr_len);
-            
+
             if (recv_len < 0) {
                 if (errno != EAGAIN && errno != EWOULDBLOCK) {
                     perror("Receive failed");
